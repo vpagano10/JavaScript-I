d9c2bfeed16bb503582f655c4155eaa6e0235f32
vpagano10 <vpagano10@gmail.com>
2019-09-17 19:38:13 -0400

    commit fixing some issues

--- assignments/objects.js
+++ assignments/objects.js
@@ -121,8 +121,8 @@ console.log(parent.child.age);
 // Log the name and age of the grandchild
 console.log(parent.child.grandchild.age);
 // Have the parent speak
-console.log(parent.speak);
+console.log(parent.speak());
 // Have the child speak
-console.log(parent.child.speak);
+console.log(parent.child.speak());
 // Have the grandchild speak
-console.log(parent.child.grandchild.speak);
+console.log(parent.child.grandchild.speak());
